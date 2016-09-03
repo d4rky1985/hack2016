7e247c7653bdeb43b70f636d15eaf348429f7b3c
Dan Paun <dan.paun@emag.ro>
2016-09-03 14:31:21 +0300

    entities

---
+++ src/AppBundle/Entity/ProductsBought.php
@@ -0,0 +1,134 @@
+<?php
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * ProductsBought
+ *
+ * @ORM\Table(name="products_bought", indexes={@ORM\Index(name="FK_products_bought_products", columns={"product_id"}), @ORM\Index(name="FK_products_bought_products_2", columns={"parent_id"})})
+ * @ORM\Entity
+ */
+class ProductsBought
+{
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="buying_date", type="datetime", nullable=false)
+     */
+    private $buyingDate;
+
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="IDENTITY")
+     */
+    private $id;
+
+    /**
+     * @var \AppBundle\Entity\Products
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Products")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="product_id", referencedColumnName="id")
+     * })
+     */
+    private $product;
+
+    /**
+     * @var \AppBundle\Entity\Products
+     *
+     * @ORM\ManyToOne(targetEntity="AppBundle\Entity\Products")
+     * @ORM\JoinColumns({
+     *   @ORM\JoinColumn(name="parent_id", referencedColumnName="id")
+     * })
+     */
+    private $parent;
+
+
+
+    /**
+     * Set buyingDate
+     *
+     * @param \DateTime $buyingDate
+     *
+     * @return ProductsBought
+     */
+    public function setBuyingDate($buyingDate)
+    {
+        $this->buyingDate = $buyingDate;
+
+        return $this;
+    }
+
+    /**
+     * Get buyingDate
+     *
+     * @return \DateTime
+     */
+    public function getBuyingDate()
+    {
+        return $this->buyingDate;
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set product
+     *
+     * @param \AppBundle\Entity\Products $product
+     *
+     * @return ProductsBought
+     */
+    public function setProduct(\AppBundle\Entity\Products $product = null)
+    {
+        $this->product = $product;
+
+        return $this;
+    }
+
+    /**
+     * Get product
+     *
+     * @return \AppBundle\Entity\Products
+     */
+    public function getProduct()
+    {
+        return $this->product;
+    }
+
+    /**
+     * Set parent
+     *
+     * @param \AppBundle\Entity\Products $parent
+     *
+     * @return ProductsBought
+     */
+    public function setParent(\AppBundle\Entity\Products $parent = null)
+    {
+        $this->parent = $parent;
+
+        return $this;
+    }
+
+    /**
+     * Get parent
+     *
+     * @return \AppBundle\Entity\Products
+     */
+    public function getParent()
+    {
+        return $this->parent;
+    }
+}
--- src/AppBundle/Entity/User.php
+++ src/AppBundle/Entity/User.php
@@ -17,9 +17,72 @@ class User extends BaseUser
      */
     protected $id;
 
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="sex", type="string", length=50, nullable=true)
+     */
+    private $sex;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="birthday", type="datetime", nullable=true)
+     */
+    private $birthday;
+
     public function __construct()
     {
         parent::__construct();
         // your own logic
+    }
+
+
+    /**
+     * Set sex
+     *
+     * @param string $sex
+     *
+     * @return Users
+     */
+    public function setSex($sex)
+    {
+        $this->sex = $sex;
+
+        return $this;
+    }
+
+    /**
+     * Get sex
+     *
+     * @return string
+     */
+    public function getSex()
+    {
+        return $this->sex;
+    }
+
+    /**
+     * Set birthday
+     *
+     * @param \DateTime $birthday
+     *
+     * @return Users
+     */
+    public function setBirthday($birthday)
+    {
+        $this->birthday = $birthday;
+
+        return $this;
+    }
+
+    /**
+     * Get birthday
+     *
+     * @return \DateTime
+     */
+    public function getBirthday()
+    {
+        return $this->birthday;
     }
 }
