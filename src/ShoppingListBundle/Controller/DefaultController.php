d1e45d7e0292d7ef8c48d16c14833294395d1a98
Alin Barbu <alin.barbu@emag.ro>
2016-09-03 21:48:02 +0300

    Add js popup product from Facebook

--- src/ShoppingListBundle/Controller/DefaultController.php
+++ src/ShoppingListBundle/Controller/DefaultController.php
@@ -4,14 +4,20 @@ namespace ShoppingListBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/")
-     */
     public function indexAction()
     {
         return $this->render('ShoppingListBundle:Default:index.html.twig');
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function addProductAjaxAction()
+    {
+        return new JsonResponse(array('success' => true));
     }
 }
